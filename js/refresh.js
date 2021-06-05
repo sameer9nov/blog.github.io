40cebfe80b54e8b681ddbe70ec2a698919262e14
unknown <sameer9nov@gmail.com>
2021-06-05 11:50:43 +0000

    Comment Changes Test

--- js/refresh.js
+++ js/refresh.js
@@ -208,3 +208,8 @@ $(document).ready(function () {
             }
         });
 })
+
+function changeValue(elementName, newValue){
+  document.getElementsByName(elementName)[0].value=newValue;
+  window.location.hash = "#comment-form";
+};
