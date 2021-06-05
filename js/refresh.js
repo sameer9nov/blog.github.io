9f5ad19c99d9afbdc1f807771b8d53fc0441376c
unknown <sameer9nov@gmail.com>
2021-06-05 14:52:26 +0000

    Provide option to comment

--- js/refresh.js
+++ js/refresh.js
@@ -208,8 +208,3 @@ $(document).ready(function () {
             }
         });
 })
-
-function changeValue(elementName, newValue){
-  document.getElementsByName(elementName)[0].value=newValue;
-  window.location.hash = "#comment-form";
-};
